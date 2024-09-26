b7053dec7ac0f2d19821b161dd63b3a68999f8f7
kryparnold <emirhanpgt@gmail.com>
2024-09-26 16:09:21 +0300

    init drizzle with postgres for authjs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
+import { database } from "@/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: DrizzleAdapter(database),
 	providers: [],
 });
