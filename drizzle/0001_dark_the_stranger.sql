f7f8c8c5dc949a30468cc0bcb6462d1ced4a2afc
kryparnold <emirhanpgt@gmail.com>
2024-09-26 16:26:20 +0300

    add messages table and relations

---
+++ drizzle/0001_dark_the_stranger.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "message" (
+	"id" text PRIMARY KEY NOT NULL,
+	"userId" text NOT NULL,
+	"text" text NOT NULL,
+	"channelId" text NOT NULL,
+	"createdAt" timestamp NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "message" ADD CONSTRAINT "message_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
