eae2bdb4ff4bf29d69183d4e3cfa55e46ff517f4
kryparnold <emirhanpgt@gmail.com>
2024-09-26 22:17:59 +0300

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <div>
-            Hello World!
+        <div className="grid place-items-center flex-1 px-2">
+            <div className="text-center flex flex-col gap-2">
+                <div className="font-semibold text-2xl">
+                    NoYouChat <span className="font-normal">&#39;e hoşgeldin.</span>
+                </div>
+                <div className="text-xl text-balance">
+                    Sohbet odaları oluşturabilir, katılabilir ve istediğiniz kadar sohbet edebilirsiniz.
+                </div>
+                <div className="flex gap-2 justify-center">
+                    <Link className={buttonVariants()} href={"/chats"}>
+                        Odalar
+                    </Link>
+                    <Link className={buttonVariants({
+                        variant: "outline"
+                    })} href={"/chats/new"}>
+                        Oda Oluştur
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
