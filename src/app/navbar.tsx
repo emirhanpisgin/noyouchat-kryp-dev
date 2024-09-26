4802923d93ae5b65606524fbde31cc777f326190
kryparnold <emirhanpgt@gmail.com>
2024-09-26 23:12:46 +0300

    styling changes

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,15 +1,16 @@
 import { auth } from "@/lib/auth";
 import ProfileButton from "./profile-button";
 import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
 
 export default async function Navbar() {
     const session = await auth();
 
     return (
         <div className="flex items-center border-b p-4">
-            <div className="text-3xl font-semibold flex-1">
+            <Link href={"/"} className="text-3xl font-semibold flex-1">
                 NoYouChat
-            </div>
+            </Link>
             <div className="flex gap-2">
                 <ModeToggle />
                 <ProfileButton user={session?.user} />
