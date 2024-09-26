e4253d104d76cb844da4e722078557add9e82820
kryparnold <emirhanpgt@gmail.com>
2024-09-26 16:19:30 +0300

    add google provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,9 @@
 import { database } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: DrizzleAdapter(database),
-	providers: [],
+	providers: [Google],
 });
