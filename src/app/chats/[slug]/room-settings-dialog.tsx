2d9b1b3bab2cc3ae75129a98a89b74e540bf400b
kryparnold <emirhanpgt@gmail.com>
2024-09-28 23:32:06 +0300

    add deleting room action

--- src/app/chats/[slug]/room-settings-dialog.tsx
+++ src/app/chats/[slug]/room-settings-dialog.tsx
@@ -1,20 +1,21 @@
 "use client";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ChatRoom } from "@/db/schema";
-import { Settings, Check, Save } from "lucide-react";
+import { Settings, Check, Save, LoaderCircle } from "lucide-react";
 import { useState } from "react";
 import { useServerAction } from "zsa-react";
-import { editRoomNameAction } from "./actions";
+import { deleteRoomAction, editRoomNameAction } from "./actions";
 import { useToast } from "@/hooks/use-toast";
 
 export default function RoomSettingsDialog({ room }: { room: ChatRoom }) {
     const [roomName, setRoomName] = useState(room.name);
     const [showCheckmark, setShowCheckmark] = useState(false);
+    const [confirmScreen, setConfirmScreen] = useState(false);
     const { toast } = useToast();
-    const { isPending, execute } = useServerAction(editRoomNameAction, {
+    const { isPending: isEditPending, execute: executeRename } = useServerAction(editRoomNameAction, {
         onSuccess() {
             toast({
                 title: "Başarılı",
@@ -24,6 +25,8 @@ export default function RoomSettingsDialog({ room }: { room: ChatRoom }) {
             setTimeout(() => setShowCheckmark(false), 2000);
         }
     });
+
+    const { isPending: isDeletePending, execute: executeDelete } = useServerAction(deleteRoomAction, {})
 
     return (
         <Dialog>
@@ -38,18 +41,47 @@ export default function RoomSettingsDialog({ room }: { room: ChatRoom }) {
                     <DialogTitle>Oda Ayarları</DialogTitle>
                 </DialogHeader>
                 <div className="flex flex-col gap-2">
-                    <Label>
-                        Oda İsmi
-                        <div className="mt-2 flex gap-2">
-                            <Input disabled={isPending} className="flex-1" placeholder="Oda İsmi..." value={roomName} onChange={(e) => setRoomName(e.target.value)} />
-                            <Button size={"icon"} className="overflow-hidden disabled:opacity-100" disabled={isPending || !roomName.length || roomName === room.name} onClick={() => execute({
+                    <div className="flex flex-col gap-2">
+                        <Label>
+                            Oda İsmi
+                        </Label>
+                        <div className="flex gap-2">
+                            <Input disabled={isEditPending} className="flex-1" placeholder="Oda İsmi..." value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+                            <Button size={"icon"} className="overflow-hidden disabled:opacity-100" disabled={isEditPending || !roomName.length || roomName === room.name} onClick={() => executeRename({
                                 roomId: room.id,
                                 name: roomName
                             })}>
-                                {showCheckmark ? <Check className="animate-peek text-green-500"/> : <Save />}
+                                {showCheckmark ? <Check className="animate-peek text-green-500" /> : <Save />}
                             </Button>
                         </div>
-                    </Label>
+                    </div>
+                    {confirmScreen ? (
+                        <div className="flex flex-col gap-2">
+                            <Label>
+                                Odayı Silmek istediğine emin misin?
+                                Bu işlem geri alınamaz!
+                            </Label>
+                            <div className="flex gap-2">
+                                <Button variant={"outline"} disabled={isDeletePending} onClick={() => setConfirmScreen(false)}>
+                                    İptal
+                                </Button>
+                                <Button variant={"outline"} disabled={isDeletePending} className="text-red-500" onClick={() => executeDelete({
+                                    roomId: room.id
+                                })}>
+                                    {isDeletePending ? <LoaderCircle className="animate-spin" /> : "Sil"}
+                                </Button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col gap-2">
+                            <Label>
+                                Odayı Sil
+                            </Label>
+                            <Button variant={"outline"} className="text-red-500" onClick={() => setConfirmScreen(true)}>
+                                Sil
+                            </Button>
+                        </div>
+                    )}
                 </div>
             </DialogContent>
         </Dialog>
