35cd55535723e687f585390e61ac94543603d644
kryparnold <emirhanpgt@gmail.com>
2024-09-28 03:11:04 +0300

    add new room dialog to top

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -2,14 +2,16 @@ import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/ca
 import { getRoomsWithAuthorsUseCase } from "@/use-cases/chat-rooms";
 import { User } from "lucide-react";
 import Link from "next/link";
+import NewRoomDialog from "../new-room-dialog";
 
 export default async function Chats() {
     const rooms = await getRoomsWithAuthorsUseCase();
 
     return (
         <div className="px-2 mx-auto w-full text-center">
-            <div className="text-3xl my-4 font-semibold">
+            <div className="text-3xl my-4 font-semibold flex items-center gap-3 justify-center">
                 Sohbet Odaları
+                <NewRoomDialog />
             </div>
             <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 lg:px-8">
                 {rooms.map((room) => (
