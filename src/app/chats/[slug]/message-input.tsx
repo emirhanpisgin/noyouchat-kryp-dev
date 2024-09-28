5be02ce1ec1b1851fee09d5fa5fcf9cd62cf2e29
kryparnold <emirhanpgt@gmail.com>
2024-09-28 03:14:48 +0300

    add message input component to create new messages with server actions

---
+++ src/app/chats/[slug]/message-input.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { AutosizeTextarea, AutosizeTextAreaRef } from "@/components/ui/autosize-textarea";
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import { useServerAction } from "zsa-react";
+import { createMessageAction } from "./actions";
+import { LoaderCircle, SendHorizontal } from "lucide-react";
+
+export default function MessageInput({ roomId }: { roomId: string }) {
+    const [message, setMessage] = useState("");
+    const inputRef = useRef<HTMLTextAreaElement | null>(null);
+    const { data, error, execute, isPending } = useServerAction(createMessageAction, {
+        onFinish() {
+            setMessage("");
+            inputRef.current?.focus();
+        }
+    });
+
+    return (
+        <div className="border-t flex p-2 gap-2">
+            <AutosizeTextarea ref={inputRef as React.RefObject<AutosizeTextAreaRef>} autoFocus className="resize-none font-medium flex-1" value={message} onChange={(e) => setMessage(e.target.value)} disabled={isPending} placeholder="Mesaj..." />
+            <Button size={"icon"} className="m-[1px]" disabled={isPending} onClick={() => execute({
+                message,
+                roomId
+            })}>
+                {isPending ? <LoaderCircle className="animate-spin"/> : <SendHorizontal />}
+            </Button>
+        </div>
+    );
+}
