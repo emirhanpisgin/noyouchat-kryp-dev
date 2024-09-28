6708d13ac4e56a27bf1068575203124bcac57828
kryparnold <emirhanpgt@gmail.com>
2024-09-28 03:12:55 +0300

    add a method to get user by id

--- src/data-access/users.ts
+++ src/data-access/users.ts
@@ -1,7 +1,13 @@
 import { database } from "@/db";
-import { UpdateUser, users } from "@/db/schema";
+import { UpdateUser, User, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export async function updateUser(data: UpdateUser) {
-    await database.update(users).set(data).where(eq(users.id, data.id));
+	await database.update(users).set(data).where(eq(users.id, data.id));
+}
+
+export async function getUserById(userId: string) {
+	return (await database.query.users.findFirst({
+		where: eq(users.id, userId),
+	}) as User)
 }
