526a435832f95dd16b228828aa46d6a75b6c3adb
kryparnold <emirhanpgt@gmail.com>
2024-09-28 03:10:52 +0300

    add revalidatepath

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,16 @@
 "use server";
 import { authenticatedAction } from "@/lib/safe-action";
 import { createChatRoomUseCase } from "@/use-cases/chat-rooms";
+import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
 export const createChatRoomAction = authenticatedAction
 	.createServerAction()
 	.input(z.string())
 	.handler(async ({ ctx, input }) => {
-		return await createChatRoomUseCase(ctx.id!, input);
+		const newRoom = await createChatRoomUseCase(ctx.id!, input);
+
+        revalidatePath("/chats");
+
+        return newRoom;
 	});
