95e2560e8b62f4232a5b78e8a17bb7434f425918
kryparnold <emirhanpgt@gmail.com>
2024-09-29 23:33:47 +0300

    fix uploading user images

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,32 @@
 import { database } from "@/db";
 import { uploadUserImage } from "@/helpers/uploadthing";
-import { updateUserUseCase } from "@/use-cases/users";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Discord from "next-auth/providers/discord";
+import { updateUserUseCase } from "@/use-cases/users";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: DrizzleAdapter(database),
-	providers: [Google,Discord],
+	providers: [Google, Discord],
 	callbacks: {
-		async signIn({ user }) {
-			const userImage = user.image;
+		async session({ session, user }) {
+			if (session.user && user.image) {
+				const userImage = session.user.image;
 
-			if (!user.image?.includes("utfs.io")) {
-				const uploadedImage = await uploadUserImage(userImage!, user.id!);
+				if (!userImage?.includes("utfs.io") && userImage) {
+					const uploadedImage = await uploadUserImage(userImage, user.id);
 
-				await updateUserUseCase({
-					id: user.id!,
-					image: uploadedImage,
-				});
+					await updateUserUseCase({
+						id: user.id,
+						image: uploadedImage,
+					});
+
+					session.user.image = uploadedImage;
+				}
 			}
 
-			return true;
+			return session;
 		},
 	},
 });
